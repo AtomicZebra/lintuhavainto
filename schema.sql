551e28a53da4b40ee87b998fee9379e08ea3be70
atomiczebra <atomiczebra@icloud.com>
2025-09-21 00:27:27 +0300

    SQL Table file

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    bird_species TEXT,
+    kunta TEXT,
+    location TEXT,
+    additional_info TEXT,
+    user_id INTEGER REFERENCES users,
+    time_added TEXT
+);
