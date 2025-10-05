66f6b401605938bd1b6731c4a832883ff1ae713d
atomiczebra <atomiczebra@icloud.com>
2025-10-05 20:00:55 +0300

    Add user page and refactor login/registration

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import check_password_hash, generate_password_hash
 import db
 import config
 import sightings
+import users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
@@ -30,6 +30,14 @@ def check_requirements(data, length: int, required: bool):
 def index():
     all_sightings = sightings.get_sighting()
     return render_template("index.html", sighting=all_sightings)
+
+@app.route("/user/<int:user_id>")
+def show_user(user_id):
+    user = users.get_user(user_id)
+    if not user:
+        abort(404)
+    sighting = users.get_sighting(user_id)
+    return render_template("show_user.html", user=user, sighting = sighting)
 
 @app.route("/sight/<int:sight_id>")
 def show_sight(sight_id):
@@ -132,11 +140,9 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
-
+    
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
 
@@ -151,12 +157,9 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        result = db.query(sql, [username])[0]
-        user_id = result["id"]
-        password_hash = result["password_hash"]
 
-        if check_password_hash(password_hash, password):
+        user_id = users.check_login(username, password)
+        if user_id:
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
